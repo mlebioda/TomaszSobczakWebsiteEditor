61a91f6f13b642aa7a0fdacdcc4ccc503fbf16d3
Michal Lebioda <m19lebioda96@gmail.com>
2021-09-12 21:33:05 +0200

    new project

---
+++ Strona/Model/Dropdown.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strona.Model
+{
+    public class Dropdown
+    {
+        List<Filter> m_filters;
+        Ul m_ul;
+
+
+
+    }
+}
---
+++ Strona/Model/Filter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strona.Model
+{
+    public class Filter
+    {
+        string m_caption;
+        string m_tag;
+    }
+}
